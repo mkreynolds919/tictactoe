89aea0357651fb257f1e81d062f5870bbef4eb24
mkreynolds919 <mkreynolds919@gmail.com>
2025-01-05 00:24:23 -0600

    Create player factory function and change gameBoard creation to attribution not function

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,23 @@
 
 const ticTacToe = (function () {
-    const gameBoard = () => [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    return {gameBoard};
+    const gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const player = function (inputName, inputToken) {
+        const name = inputName;
+        const token = inputToken;
+        const placeToken = function (index) {
+            gameBoard.splice(index-1, 1, token);
+        }
+        return {name, token, placeToken};
+    }
+    return {gameBoard, player};
 })();
 
+const player1 = ticTacToe.player("Player1", "X");
+const player2 = ticTacToe.player("Player2", "O");
+
+player1.placeToken(1);
+player2.placeToken(4);
+
+console.log(ticTacToe.gameBoard);
 
