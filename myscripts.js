83b275f99889b9437bea290845dd7b47fd96106e
mkreynolds919 <mkreynolds919@gmail.com>
2025-01-06 01:41:53 -0600

    Convert placeToken method as its own method, separate from player
    Instantiate a currPlayer variable that handles turn logic
    
    Add comments for explanations

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,14 @@
 
+//Initiates the ticTacToe class//
 const ticTacToe = (function () {
+    //Initiates gameBoard, uses IIFE//
     const gameBoard = (function () {
         var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         const getBoard = () => board;
         return {getBoard};
     })();
 
+    //Used for checkWin, simply compares to same-size arrays to see if they are identical//
     const compareResults = function (arr1, arr2) {
         for (var i = 0; i < arr1.length; i++) {
             if (arr1[i] != arr2[i]) return false;
@@ -13,6 +16,7 @@ const ticTacToe = (function () {
         return true;
     }
 
+    //Is called after every placement, compares current gameboard status with potential winning three-in-a-rows//
     const checkWin = function (gmBrd, tkn) {
         let win = false;
         if (compareResults([gmBrd[0], gmBrd[1], gmBrd[2]], [tkn, tkn, tkn])) win = true;
@@ -26,25 +30,48 @@ const ticTacToe = (function () {
         return win;
     }
 
+    //takes the current token of whomever's turn it is, places it at index//
+    const placeToken = function (index) {
+        //mutates the gameboard array by adding the currPlayer's token at index//
+        gameBoard.getBoard().splice(index-1, 1, currPlayer.token);
+
+        //Outputs current state of the gameboard after the placement//
+        console.log(gameBoard.getBoard());
+
+        //checks win conditions with method, gives output message if they won//
+        if (checkWin(gameBoard.getBoard(), currPlayer.token)) {
+            console.log(currPlayer.name + " has won the game!");
+        } else {
+        
+            //Assigns currPlayer to the opposite of whomever just went//
+            if (currPlayer.name == player1.name) {
+                currPlayer = player2;
+            } else {
+                currPlayer = player1;
+            }
+
+            //Outputs message letting user know who's turn it is//
+            console.log(currPlayer.name + ", it's your turn now!");
+        }
+    }
+
+    //Factory function for player, simply stores name and chosen token//
     const player = function (inputName, inputToken) {
         const name = inputName;
         const token = inputToken;
-        const placeToken = function (index) {
-            gameBoard.getBoard().splice(index-1, 1, token);
-            if (checkWin(gameBoard.getBoard(), token)) {
-                console.log(name + " has won the game!");
-            }
-        }
-        return {name, token, placeToken};
+        
+        return {name, token};
     }
 
+    //Initializes two players//
     const player1 = player("Player1", "X");
     const player2 = player("Player2", "O");
 
-    return {gameBoard, player1, player2};
-})();
+    //Initializes first player's turn//
+    var currPlayer = player1;
 
-ticTacToe.player1.placeToken(1);
-console.log(ticTacToe.gameBoard.getBoard());
+    //returns these methods for mutabilitiy and access//
+    return {gameBoard, placeToken, player1, player2, currPlayer};
+})();
 
 
