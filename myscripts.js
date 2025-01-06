e256b5b96688e0c29a2a0201bec46909220dbade
mkreynolds919 <mkreynolds919@gmail.com>
2025-01-06 00:12:14 -0600

    Modify board method into an IIFE and add the rest of win cases in checkWin method

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,28 @@
 
 const ticTacToe = (function () {
-    const gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const gameBoard = (function () {
+        var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const getBoard = () => board;
+        return {getBoard};
+    })();
+
+    const compareResults = function (arr1, arr2) {
+        for (var i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
 
     const checkWin = function (gmBrd, tkn) {
         let win = false;
-        if (gmBrd[0] == tkn && gmBrd[1] == tkn && gmBrd[2] == tkn) {
-            win = true;
-        }
+        if (compareResults([gmBrd[0], gmBrd[1], gmBrd[2]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[3], gmBrd[4], gmBrd[5]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[6], gmBrd[7], gmBrd[8]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[0], gmBrd[3], gmBrd[6]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[1], gmBrd[4], gmBrd[7]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[2], gmBrd[5], gmBrd[8]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[0], gmBrd[4], gmBrd[8]], [tkn, tkn, tkn])) win = true;
+        if (compareResults([gmBrd[2], gmBrd[4], gmBrd[6]], [tkn, tkn, tkn])) win = true;
         return win;
     }
 
@@ -14,8 +30,8 @@ const ticTacToe = (function () {
         const name = inputName;
         const token = inputToken;
         const placeToken = function (index) {
-            gameBoard.splice(index-1, 1, token);
-            if (checkWin(gameBoard, token)) {
+            gameBoard.getBoard().splice(index-1, 1, token);
+            if (checkWin(gameBoard.getBoard(), token)) {
                 console.log(name + " has won the game!");
             }
         }
@@ -28,6 +44,7 @@ const ticTacToe = (function () {
     return {gameBoard, player1, player2};
 })();
 
-
+ticTacToe.player1.placeToken(1);
+console.log(ticTacToe.gameBoard.getBoard());
 
 
